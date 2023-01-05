b653533aa88056c8ce7f1a33b87a5661eca5daf0
Hamzanazir685 <hamzanazir685@gmail.com>
2023-01-06 01:33:10 +0500

    Added Comment

--- src/navigation/HomeTabNavigator.js
+++ src/navigation/HomeTabNavigator.js
@@ -7,6 +7,7 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Feather from 'react-native-vector-icons/Feather';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
+// Comments to show
 const Tab = createBottomTabNavigator();
 
 const HomeTabNavigator = props => {
